8894d611ef958076ec0355411506ceed9333af60
Ife Abraham <ife04abraham@gmail.com>
2026-05-11 08:06:55 +0100

    updates for server config

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,6 @@
 ﻿import axios from 'axios';
 
 // const API = axios.create({
-//   baseURL: 'http://localhost:5000/api',
-//   timeout: 10000,
-// });
-// const API = axios.create({
 //   baseURL: 'https://realtiq-backend.onrender.com/api',
 //   timeout: 10000,
 // });
@@ -12,6 +8,10 @@ const API = axios.create({
   baseURL: 'https://api.realtiq.com.ng/api',
   timeout: 10000,
 });
+// const API = axios.create({
+//   baseURL: 'http://localhost:5000/api',
+//   timeout: 10000,
+// });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
--- vite.config.ts
+++ vite.config.ts
@@ -13,3 +13,14 @@ export default defineConfig({
     },
   },
 })
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// })
